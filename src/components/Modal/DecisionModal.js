cc8060bdf23d243f1af3486783306632bb04dae8
kushalbabay <kushalbabay@gmail.com>
2021-03-03 14:24:06 +0530

    created modal components, fixed viewquestion

---
+++ src/components/Modal/DecisionModal.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { XSquare } from 'react-bootstrap-icons'
+import classes from './modal.module.css'
+
+export default function DecisionModal(props) {
+    const { hideModal } = props
+    React.useEffect(() => {
+        document.addEventListener('keydown', (e) => {
+            if (e.key === 'Escape') {
+                hideModal()
+            }
+        })
+    }, [hideModal])
+    return (
+        <div className={classes.modal}>
+            <div className={props.modalIsOpen ? classes.backdrop : classes.backdrop + " " + classes.backdropHidden}>
+                <div className={props.modalIsOpen ? classes.modalBody + ' ' + classes.modalOpen : classes.modalBody}>
+                    <div className='bg-light font-weight-bold p-2 w-100 d-flex justify-content-between'>{props.title}
+                        <XSquare className={classes.closeBtn} onClick={props.hideModal} />
+                    </div>
+                    <div className='bg-white'>
+                        {props.children}
+                    </div>
+                    <div className='bg-light text-center p-3'>
+                        {props.confirmationMessage}
+                        <br/>
+                        <br/>
+                    <button onClick={props.hideModal} className="btn btn-primary mr-3">Cancel</button>
+                    <button onClick={() => {hideModal() ;props.confirmActionHandler(props.id)}} className="btn btn-danger">Confirm</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/containers/ViewQuestion/ViewQuestion.js
+++ src/containers/ViewQuestion/ViewQuestion.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { CheckCircle } from 'react-bootstrap-icons'
 
 export default function ViewQuestion(props) {
-    let options = Array.isArray(props.state.options) ? props.state.options : props.state.options[props.state.type]
+    let source = props.state.fields ? props.state.fields : props.state
+    let options = Array.isArray(source.options) ? source.options : source.options[source.type]
     let optionList = options.map((option, index) => <div key={index} 
             className={option[1] ? "input-group border rounded border-success mb-3" : "input-group mb-3"} >
         <div className="input-group-prepend">
@@ -9,30 +11,32 @@ export default function ViewQuestion(props) {
                 {String.fromCharCode(index+65)}
             </div>
         </div>
-        <div className='form-control  w-75' htmlFor={index}>
+        <div className='form-control  w-75'>
             {option[0]} 
             {option[1] ? <span className='text-success float-right'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
-                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
-            </svg>
+            <CheckCircle style={{fontSize:'1.2rem'}} />
             </span> : null} 
         </div>
     </div>)
-    //let answers ="[ "+ options.filter(option => option[1]).map(option => option[0]) + " ]"
     return (
         <div className='border p-3'>
-            <h4 className='lead'><span className='font-weight-bold'>Question: &nbsp;</span>{props.state.text}</h4>
-            <br/>
-            <h4 className='font-weight-bold lead'>Options: </h4>
-            <div className="mt-3 pl-3">
-            {optionList}
-            </div>
-            <div className='d-flex mt-4 justify-content-between'>
-                <h4 className='lead'><span className='font-weight-bold'>Complexity: &nbsp;</span>{props.state.complexity.value}</h4>
-                <h4 className='lead'><span className='font-weight-bold'>Time: &nbsp;</span>{props.state.time.value} min(s)</h4>
+
+            <div className="row px-3">
+                <span className='lead font-weight-bold h4'>Question: &nbsp;</span>
+                <h4 className='lead' dangerouslySetInnerHTML={{__html: source.text}}></h4>
             </div>
 
+            <div className="row px-3">
+                <h4 className='font-weight-bold lead'>Options: </h4>
+                <div className="mt-3 w-100 pl-3">
+                {optionList}
+                </div>
+            </div>
+
+            <div className='row px-3 d-flex mt-4 justify-content-between'>
+                <h4 className='lead'><span className='font-weight-bold'>Complexity: &nbsp;</span>{source.complexity.value}</h4>
+                <h4 className='lead'><span className='font-weight-bold'>Time: &nbsp;</span>{source.time.value} min(s)</h4>
+            </div>
         </div>
     )
 }
